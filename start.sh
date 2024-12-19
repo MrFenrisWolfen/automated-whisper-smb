5046237f97f52390b5ccab5270dc9ad1e7960f68
MrFenrisWolfen <44942583+MrFenrisWolfen@users.noreply.github.com>
2024-12-19 14:18:11 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,18 +1,10 @@
 #!/bin/bash
 
 # Setze die Umgebungsvariable für das Modell
-export MODEL=${WHISPER_MODEL:-medium}
+export MODEL=${MODEL:-medium}
 
 # Füge den Cronjob hinzu (falls noch nicht vorhanden)
 (crontab -l 2>/dev/null; echo "*/1 * * * * MODEL=$MODEL /bin/bash /check.sh >> /var/log/check.log 2>&1") | crontab -
 
 # Starte den Cron-Dienst im Vordergrund
 cron -f
-
-
-
-
-#!/bin/bash
-
-# Starte den Cron-Dienst direkt im Vordergrund
-#cron -f
