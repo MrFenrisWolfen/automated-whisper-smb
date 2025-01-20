b91a84d864017b03b0a6f40b29f6b590911d3fab
MrFenrisWolfen <44942583+MrFenrisWolfen@users.noreply.github.com>
2025-01-20 11:58:06 +0100

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-# Pfade definieren
+# pfade definieren
 VOLUME_DIR="/app/audio/data/0_settings/"
 SCRIPT_SRC="/check.sh"
 SCRIPT_DEST="$VOLUME_DIR/check.sh"
 SETTINGS_FILE="$VOLUME_DIR/settings.txt"
 
-# Volume sicherstellen
+# volume erstellen
 mkdir -p "$VOLUME_DIR"
 
-# Script ins Volume kopieren
+# script ins volume kopieren
 cp "$SCRIPT_SRC" "$SCRIPT_DEST"
 chmod +x "$SCRIPT_DEST"
 
@@ -20,16 +20,22 @@ if [ ! -f "$SETTINGS_FILE" ]; then
 MODEL=medium
 # Sprachen (Komma-separiert, z.B. en,de)
 LANGUAGES=de,en
+# Benutzerdefinierter befehl der statt dem standard Befehl ausgeführt werden soll per Datei
+# (nur zu debugging zwecken, Befehl anpassen und Raute entfernen)
+# CUSTOM_COMMAND="whisper \"$TARGET_FILE\" --model $MODEL --language $LANGUAGE --output_dir \"$OUTPUT_DIR\""
 EOL
 fi
 
 # Crontab aktualisieren
-(
-    echo "PATH=/usr/local/bin:/usr/bin:/bin"
-    echo "SHELL=/bin/bash"
-    crontab -l 2>/dev/null
-    echo "*/1 * * * * /bin/bash $SCRIPT_DEST >> /var/log/check.log 2>&1"
-) | crontab -
+if ! crontab -l 2>/dev/null | grep -qF "$CRON_JOB"; then
+    # Crontab aktualisieren, wenn der Eintrag nicht existiert
+    (
+        echo "PATH=/usr/local/bin:/usr/bin:/bin"
+        echo "SHELL=/bin/bash"
+        crontab -l 2>/dev/null
+        echo "$CRON_JOB"
+    ) | crontab -
+fi
 
 # Debug: Aktuelle Crontab ausgeben
 crontab -l
