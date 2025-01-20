6a0b7ffe1d5526d03b6c6c078ce9805d13d453ef
MrFenrisWolfen <44942583+MrFenrisWolfen@users.noreply.github.com>
2025-01-20 14:05:45 +0100

    Update check_for_new_files.sh

--- scripts/check_for_new_files.sh
+++ scripts/check_for_new_files.sh
@@ -3,10 +3,16 @@
 # pfad des scripts
 SCRIPT_DIR=$(dirname "$(realpath "$0")")
 
+# timestamp
+echo " " >> $SCRIPT_DIR/log/check.log
+echo "==========================================" >> $SCRIPT_DIR/log/check.log
+echo " Durchgang gestartet: $(date '+%Y-%m-%d %H:%M:%S')" >> $SCRIPT_DIR/log/check.log
+echo "==========================================" >> $SCRIPT_DIR/log/check.log
+
 # prüfen ob die Lock-Datei existiert und abbruch falls ja
 SCRIPT_LOCK="$SCRIPT_DIR/script.lock"
 if [ -f "$SCRIPT_LOCK" ]; then
-  echo "Das Script ist bereits in Verwendung, stoppe erneutes Starten."
+  echo "Das Script ist bereits in Verwendung, stoppe erneutes Starten." >> $SCRIPT_DIR/log/check.log
   exit 0
 fi
 
@@ -22,8 +28,15 @@ fi
 # standardwerte falls keine settings vorhanden sein sollten
 MODEL="${MODEL:-tiny}"
 LANGUAGES="${LANGUAGES:-de}"
-echo "MODEL ist: $MODEL" >> /var/log/check.log
-echo "LANGUAGES ist: $LANGUAGES" >> /var/log/check.log
+echo "MODEL ist: $MODEL" >> $SCRIPT_DIR/log/check.log
+echo "LANGUAGES ist: $LANGUAGES" >> $SCRIPT_DIR/log/check.log
+
+# CUSTOM_COMMAND gesetzt?
+if [ -n "$CUSTOM_COMMAND" ]; then
+echo "CUSTOM_COMMAND ist: $CUSTOM_COMMAND" >> $SCRIPT_DIR/log/check.log
+else
+echo "Verwende Standard-Befehl" >> $SCRIPT_DIR/log/check.log
+fi
 
 #Settings und Ordner freigeben
 chmod -R 777 "$SCRIPT_DIR"
@@ -63,11 +76,11 @@ LAST_FILES=$(cat "$LAST_STATE_FILE")
 
 # Vergleiche die aktuellen und letzten Zustände
 if [ "$CURRENT_FILES" != "$LAST_FILES" ]; then
-  echo "Neue Dateien gefunden!"
+  echo "Neue Dateien gefunden!" >> $SCRIPT_DIR/log/check.log
 
   # Finde die neuen Dateien
   NEW_FILES=$(comm -13 <(echo "$LAST_FILES") <(echo "$CURRENT_FILES"))
-  echo "Neue Dateien: $NEW_FILES"
+  echo "Neue Dateien: $NEW_FILES" >> $SCRIPT_DIR/log/check.log
 
 
 # funktion um zu prüfen ob eine datei vollständig kopiert wurde
@@ -86,7 +99,7 @@ if [ "$CURRENT_FILES" != "$LAST_FILES" ]; then
 
       # Prüfen, ob die Datei vollständig kopiert wurde
       if ! is_file_complete "$TARGET_FILE"; then
-      echo "Datei wird noch kopiert: $TARGET_FILE"
+      echo "Datei wird noch kopiert: $TARGET_FILE" >> $SCRIPT_DIR/log/check.log
       continue
       fi
 
@@ -95,23 +108,36 @@ if [ "$CURRENT_FILES" != "$LAST_FILES" ]; then
 
       # Prüfen, ob die Datei bereits in Bearbeitung ist
       if [ -f "$LOCK_FILE" ]; then
-        echo "Überspringe Datei (bereits in Bearbeitung): $TARGET_FILE"
+        echo "Überspringe Datei (bereits in Bearbeitung): $TARGET_FILE" >> $SCRIPT_DIR/log/check.log
         continue
       fi
 
       # Lock-Datei erstellen
       touch "$LOCK_FILE"
-      echo "Verarbeite Datei: $TARGET_FILE"
+      echo "Verarbeite Datei: $TARGET_FILE" >> $SCRIPT_DIR/log/check.log
 
       # Eingabeverzeichnis und Dateiname ohne Endung
       TARGET_DIR=$(dirname "$TARGET_FILE")
       BASENAME=$(basename "$TARGET_FILE" | sed 's/\.[^.]*$//')
 
 		  for LANGUAGE in $(echo "$LANGUAGES" | tr ',' '\n'); do
-			  OUTPUT_DIR="$TARGET_DIR/${BASENAME}-${LANGUAGE}"
-			  COMMAND="whisper \"$TARGET_FILE\" --model $MODEL --language $LANGUAGE --output_dir \"$OUTPUT_DIR\""
-			  echo "starte verarbeitung für $LANGUAGES"
-			  eval "$COMMAND"
+		  OUTPUT_DIR="$TARGET_DIR/${BASENAME}-${LANGUAGE}"
+    		  echo "starte verarbeitung für $LANGUAGE" >> $SCRIPT_DIR/log/check.log
+
+       		  # Prüfen, ob ein benutzerdefiniertes Kommando existiert
+      		  if [ -n "$CUSTOM_COMMAND" ]; then
+        
+		  	# CUSTOM_COMMAND verwenden
+        	  	COMMAND=$(eval echo "$CUSTOM_COMMAND")
+        	  	echo "Verwende benutzerdefinierten Befehl: $COMMAND" >> $SCRIPT_DIR/log/check.log
+		  else
+    		  	# Standard command verwenden
+		  	COMMAND="whisper \"$TARGET_FILE\" --model $MODEL --language $LANGUAGE --output_dir \"$OUTPUT_DIR\""
+     		  	echo "Verwende Standard-Befehl: $COMMAND" >> $SCRIPT_DIR/log/check.log
+		  fi
+
+    		  # Ausführung
+		  eval "$COMMAND" 
 		  done
 
       # Lock-Datei entfernen
@@ -123,7 +149,7 @@ if [ "$CURRENT_FILES" != "$LAST_FILES" ]; then
   # Aktuellen Zustand speichern
   echo "$CURRENT_FILES" > "$LAST_STATE_FILE"
 else
-  echo "Keine neuen Dateien gefunden."
+  echo "Keine neuen Dateien gefunden."  >> $SCRIPT_DIR/log/check.log
 fi
 
 # Lock-Datei entfernen, wenn das Skript beendet ist
